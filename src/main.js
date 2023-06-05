73c13c02cd06a9190d51c7fd5ab29b2b008d7a06
Ilhamfzd <ilham.dar@students.amikom.ac.id>
2023-06-05 09:07:42 +0700

    Praktikum8

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '@lottiefiles/lottie-player'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
